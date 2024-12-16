85fd93c9fa521adf65ac3cb78572897a0ff7351e
develop05 <develop05@idrall.com>
2024-12-16 09:49:37 -0600

    [Feature] Add authentication and validation middleware
    - Introduced an AuthGuard to restrict access to the '/greet' endpoint.
    - Added a ValidateuserPipe for validating user input, specifically age.
    - Created AuthMiddleware to check for authorization headers.
    - Implemented LoggerMiddleware to log requests.
    - Updated UsersModule to apply the new middlewares on specific routes.
    - Expanded HelloController with additional endpoints for various responses.

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,10 +1,57 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { query, Request, Response } from 'express';
+import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 @Controller()
 export class HelloController {
   @Get('/')
   index(@Req() request: Request, @Res() response: Response) {
     response.status(200).json({ message: 'Hello World!' });
+  }
+
+  @Get('new')
+  @HttpCode(201)
+  new() {
+    return 'New';
+  }
+
+  @Get('notfound')
+  @HttpCode(404)
+  notFound() {
+    return '404: Not found';
+  }
+
+  @Get('error')
+  @HttpCode(500)
+  error() {
+    return '500: Internal server error';
+  }
+
+  @Get('ticket/:num')
+  getNumber(@Param('num', ParseIntPipe) num: number) {
+    return num + 2;
+  }
+
+  @Get('active/:status')
+  isUserActive(@Param('status', ParseBoolPipe) status: boolean) {
+    return status;
+  }
+
+  @Get('greet')
+  @UseGuards(AuthGuard)
+  greet(@Query(ValidateuserPipe) query: { name: string; age: number }) {
+    return `Hello ${query.name}, you are ${query.age} years old`;
   }
 }
