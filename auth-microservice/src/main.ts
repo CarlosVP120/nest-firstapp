1bbf521e18cd52faa1ea84a1f5a42138e5bb0d16
CarlosVp120 <carlosveryan4@gmail.com>
2024-12-17 00:18:31 -0600

    Add initial project setup files
    - Created .gitignore to exclude unnecessary files
    - Added environment variables in .env for server and JWT secret
    - Set up ESLint configuration for code quality checks
    - Included Prettier config for consistent formatting
    - Initialized package-lock.json with dependencies for the project

---
+++ auth-microservice/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { environments } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Auth-Microservice');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: environments.natsServer,
+      },
+    },
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+
+  logger.log(
+    `Auth Microservice started, listening on ${environments.natsServer}`,
+  );
+}
+bootstrap();
